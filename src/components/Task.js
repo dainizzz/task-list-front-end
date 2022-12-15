fe3982f44e1a209013c11eb5f64f6e2e2b565111
Presley <persleytieman@outlook.com>
2022-12-15 14:37:27 -0500

    added comments on what/where to change code

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,14 +3,17 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
+// Remove state from here
 const Task = ({ id, title, isComplete }) => {
   const [complete, setComplete] = useState(isComplete);
   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
 
+  // add const Task = (props) => {return inside here}
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
+        // add button using Props for is.Complete (<button onClick={isComplete}> Complete Task)
         onClick={() => setComplete(!complete)}
       >
         {title}
@@ -24,6 +27,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  // add isComplete prop thing
 };
 
 export default Task;
