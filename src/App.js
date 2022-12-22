f51246184b0aa5ae7924fb7067b57ded6f50c0ca
Presley <persleytieman@outlook.com>
2022-12-22 13:17:23 -0500

    started wave 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskList from './components/TaskList.js';
-
 import './App.css';
+import axios from 'axios';
 
+const baseUrl = 'http://localhost:5000';
+
+const apiToJson = task => {
+  const {description, id, is_complete: isComplete, title} = task;
+
+  return {description, id, isComplete, title};
+};
+
+const taskAsync = async () => {
+  try {
+    const respone = await axios.get(`${baseUrl}/tasks`);
+    return respone.data.map(apiToJson);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateAsync = async (id, markComplete) => {
+  const endpoint = markComplete ? `mark_complete` : 'mark_incomplete';
+
+  try {
+    const response = await axios.patch(`${baseUrl}/tasks/${id}/${endpoint}`);
+
+    return apiToJson(response.data.task);
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+const deleteAsync = async id => {
+  try {
+    await axios.delete(`${baseUrl}/tasks/${id}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
 const TASKS = [
   {
     id: 1,
@@ -19,7 +55,46 @@ const TASKS = [
 const App = () => {
   const [taskData, setTaskData] = useState(TASKS);
 
-  const updateTaskData = (updatedTask) => {
+  const reloadTask = async () => {
+    try {
+      const tasks = await taskAsync();
+      setTaskData(tasks);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    reloadTask();
+  }, []);
+
+  
+
+  // change updateTaskData for async
+  const updateTaskData = async id => {
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {return;}
+
+    try {
+      const newTask = await updateAsync(id, !task.isComplete);
+      setTaskData(prevTasks => {
+        return prevTasks.map(task => {
+          if (task.id === newTask.id) {
+            return newTask;
+          } else {
+            return task;
+          }
+        });
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  // need to write to async delete specific task 
+
+  
     const tasks = taskData.map((task) => {
       if (task.id === updatedTask.id) {
         return updatedTask;
